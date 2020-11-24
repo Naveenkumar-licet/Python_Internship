3699b8e843b9552da8053caab83d6ecb602907bd
Naveenkumar-licet <74892181+Naveenkumar-licet@users.noreply.github.com>
2020-11-24 11:43:14 +0530

    Add files via upload
    Day_2_Internship tasks

---
+++ Day_2_intern_naveenkumar.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Excercise_1
+a=b=c=int(input("Enter the value of a,b,c"))
+#divide a by 10
+d=a/10
+#multiply b by 50
+e=b*50
+#add c value by 60
+f=c+60
+print("The divided value is",d)
+print("The multiplied value is",e)
+print("The added value is",f)
+
+
+# In[14]:
+
+
+#Excercise_2
+H=input("Enter a 5 character string: ")
+H1=H.replace(H[3],'G')
+print("The original string is",H)
+print("The modified string is",H1)
+
+
+# In[1]:
+
+
+#Excercise_3
+a=int(input("Enter the value of a"))
+b=float(input("Enter the value of b"))
+c=float(a)
+d=int(b)
+print("The floated value is",c)
+print("The integered value is",d)
+
