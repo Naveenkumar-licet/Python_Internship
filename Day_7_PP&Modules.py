860b6a0327f7106639aa04e9250fabb8fa70bc59
Naveenkumar-licet <74892181+Naveenkumar-licet@users.noreply.github.com>
2020-11-29 13:54:26 +0530

    Add files via upload

---
+++ Day_7_PP&Modules.py
@@ -0,0 +1,21 @@
+#Excercise_1_Create a python module with list and import the module in another .py file and change the value in list
+
+from IMP import mylist
+mylist()
+
+#Excercise_2_Install pandas package
+import pandas as pd
+Data =[[2, 3, 4], [5, 6, 7]]
+res= pd.Series(Data)
+print(res)
+
+#Excercise_3_Import a module that picks random number and write a program to fetch a random number from 1 to  100 on every run
+from ran import rand
+rand()
+
+#Excercise_4_Import sys package and find the python path
+import sys
+sys.path
+
+
+
