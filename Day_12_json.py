4815b8ca35781c2f6b39600474e20c895a1727bd
Naveenkumar-licet <74892181+Naveenkumar-licet@users.noreply.github.com>
2020-12-04 21:52:44 +0530

    Add files via upload

---
+++ Day_12_json.py
@@ -0,0 +1,81 @@
+import json
+states={
+  "states": [
+    {
+      "name": "Alabama",
+      "abbreviation": "AL",
+      "area_codes": ["205", "251", "256", "334", "938"]
+    },
+    {
+      "name": "Alaska",
+      "abbreviation": "AK",
+      "area_codes": ["907"]
+    },
+    {
+      "name": "Arizona",
+      "abbreviation": "AZ",
+      "area_codes": ["480", "520", "602", "623", "928"]
+    },
+    {
+      "name": "Arkansas",
+      "abbreviation": "AR",
+      "area_codes": ["479", "501", "870"]
+    },
+    {
+      "name": "California",
+      "abbreviation": "CA",
+      "area_codes": ["209", "213", "310", "323", "408", "415", "424", "442", "510", "530", "559", "562", "619", "626", "628", "650", "657", "661", "669", "707", "714", "747", "760", "805", "818", "831", "858", "909", "916", "925", "949", "951"]
+    },
+    {
+      "name": "Colorado",
+      "abbreviation": "CO",
+      "area_codes": ["303", "719", "720", "970"]
+    },
+    {
+      "name": "Connecticut",
+      "abbreviation": "CT",
+      "area_codes": ["203", "475", "860", "959"]
+    },
+    {
+      "name": "Delaware",
+      "abbreviation": "DE",
+      "area_codes": ["302"]
+    },
+    {
+      "name": "Florida",
+      "abbreviation": "FL",
+      "area_codes": ["239", "305", "321", "352", "386", "407", "561", "727", "754", "772", "786", "813", "850", "863", "904", "941", "954"]
+    },
+    {
+      "name": "Georgia",
+      "abbreviation": "GA",
+      "area_codes": ["229", "404", "470", "478", "678", "706", "762", "770", "912"]
+    },
+    {
+      "name": "Hawaii",
+      "abbreviation": "HI",
+      "area_codes": ["808"]
+    },
+    {
+      "name": "Idaho",
+      "abbreviation": "ID",
+      "area_codes": ["208"]
+    },
+    {
+      "name": "Illinois",
+      "abbreviation": "IL",
+      "area_codes": ["217", "224", "309", "312", "331", "618", "630", "708", "773", "779", "815", "847", "872"]
+    },
+    {
+      "name": "Indiana",
+      "abbreviation": "IN",
+      "area_codes": ["219", "260", "317", "463", "574", "765", "812", "930"]
+    },
+    {
+      "name": "Iowa",
+      "abbreviation": "IA",
+      "area_codes": ["319", "515", "563", "641", "712"]
+    }],}
+print(type(states))
+
+
